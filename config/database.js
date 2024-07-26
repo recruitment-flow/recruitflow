d3cd072348954e86279cae298369bed1bb2f0930
admin <recruitmentflow2023@gmail.com>
2024-07-26 10:29:27 +0000

    added db config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,27 @@
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const pool = mysql.createPool({
-    host: DATABASE_HOST,
-    user: DATABASE_USERNAME,
-    password: DATABASE_PASSWORD,
-    database: DATABASE_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10, // Maximum number of connections in the pool
     queueLimit: 0 // Unlimited number of pending connections
   });
+
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error acquiring database connection:', err);
+    }
+    if (connection) {   
+      console.log('Connected to database');
+      connection.release();
+    }
+  }); 
   
   // Export the pool for use in other parts of your application
   module.exports = pool.promise();
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!', pool)
+app.get('/', (req, res) => {   
+    res.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`Server started on port http://localhost:${port}`))
